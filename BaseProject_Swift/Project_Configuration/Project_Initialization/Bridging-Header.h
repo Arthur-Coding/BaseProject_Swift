78b581751f6d4bc764213d2e94228a0f9de438c7
ArthurShuai <zhixingui_liushuai@163com>
2018-02-05 21:59:22 +0800

    update

---
+++ BaseProject_Swift/Project_Configuration/Project_Initialization/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  BaseProject_Swift
+//
+//  Created by ArthurShuai on 2018/2/5.
+//  Copyright © 2018年 ArthurShuai. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, ‘8.0’
+use_frameworks!
+
+target 'BaseProject_Swift' do
+   pod 'LSRouter_Swift',:inhibit_warnings => true
+end
